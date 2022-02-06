18fc4a3771a35d0d43cf6fff5f2e4dc734932997
Nitish Gudapati <gudapatinitish9@gmail.com>
2022-02-06 15:19:12 -0800

    Delete Workspace

--- project_ws/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/mahesh/me326_project_repo/Collaborative_Robotics/project_ws/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- project_ws/build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/mahesh/me326_project_repo/Collaborative_Robotics/project_ws/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
