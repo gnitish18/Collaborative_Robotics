d2f164ccdd334a995c1ab227ae63833559d3f86b
Mahesh Saboo <maheshsaboo25@gmail.com>
2022-02-07 04:04:04 +0530

    workspace setup

---
+++ project_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/mahesh/me326_project_repo/Collaborative_Robotics/project_ws/src
+# Build directory: /home/mahesh/me326_project_repo/Collaborative_Robotics/project_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ project_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/mahesh/me326_project_repo/Collaborative_Robotics/project_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
